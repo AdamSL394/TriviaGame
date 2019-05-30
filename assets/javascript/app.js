d2b8ea11939d067155e8bcdb436514417554a0c1
Adam <lehreradam@yahoo.com>
2019-05-30 14:31:18 -0700

    templet set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Button that starts the game 
+// Loads next page with a timer that counts down 
+// loads questions that will be buttons 
+//  each question will be a if statement determining what to do if the user answers
+// 
+
+
+
+
+var startgame = setTimeout(function(){
+    // $("#timer").on("click")
+     console.log("startgame",);
+},1000)
