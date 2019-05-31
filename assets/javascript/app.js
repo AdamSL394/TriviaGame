3bc1d59a5afb8a24eaf99cfb20078b272302a9a7
Adam <lehreradam@yahoo.com>
2019-05-31 16:03:40 -0700

    buttons added and cycle out each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,81 +5,108 @@
 // 
 
 var startGame;
-var correctAnswers=0;
-var incorrectAnswers=0;
-var timeRemaining=false;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var timeRemaining = false;
 var number = 5;
 var questionIndex = 0;
 
-var question= [{
-    q:"Which are National Parks ? ",
-    a:"Yosemite",
-    a:"Green",
-    a:"Red",
-    a:"Yellow",
+var question = [
+    {
+        q: "Which are National Parks ? ",
+        a: ["Yosemite", "Angel Island", "Ahjumawi Lava Springs", "Antelope Valley California Poppy "],
+        c: "Yosemite",
+        d: "hello",
+        image: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        q: "Places to go skydiving ",
+        a: ["Go Jump","IFly", "We Soar", "Eagal"],
+        c: "Go Jump",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOugwIt8_a_WdFfwY34-CfnxziuWcUvFEy4z3sdw8mKpmMwrao9Q"
 
-},{
-    q:"Places to go skydiving ",
-    a:"yes",
-    a:"No",
-    a:"Maybe",
-    a:"So",
+    },
+    {
+        q: " Best TV show ",
+        a: [" Ronald McDonald "," Big Mac ", "Game of Thrones ", " Jersey Shore "],
+        c: " Jersey Shore ",
+        image: "http://townsquare.media/site/252/files/2018/05/jersey-shore.jpg"
 
-},{
-    q:"Actual States",
-    a:"California",
-    a:"Blah",
-    a:"Yes",
-    a:"No",
 
-}
-
+    }
 ]
 
 
-
-
 $("#start").on("click", run);
- 
-
-$("#stop").on("click",stop);
 
 
-     function renderQuestion (){
-       if ( questionIndex <= question.length){
-       ($("#q1").html(question[questionIndex].q))
+$("#stop").on("click", stop);
+
+
+function renderQuestion() {
+    $("#answers").empty();
+    $(".Jumbotron").empty();
+    if (questionIndex !== 3) {
+        $("#q").html("<p>" + question[questionIndex].q + "</p>");
+        for(var i= 0; i < question[questionIndex].a.length; i++){
+            var button = $("<button>");
+            button.append(question[questionIndex].a[i])
+            button.attr("class", "answer-button")
+            button.attr("data-name", question[questionIndex].a[i])
+            $("#answers").append(button)
+
         }
-        else {$(".endGame").text("Final Score " + correctAnswers + " out of " + 5)}
-     }
+      
+    }
+    else if (questionIndex === 3){
+         $(".endGame").html("Final Score " + correctAnswers + " out of " + 3) 
+    }
+    stop()
+}
+    
+function run() {
+    clearInterval(startGame);
+    renderQuestion();
+    startGame = setInterval(decrement, 1000);
+}
 
-    function run(){
-        clearInterval(startGame);
+function decrement() {
+    number--;
+    $("#showNumber").html(number);
+    if (number === 0) {
+        stop();
+        alert("Time Up!");
+        questionIndex++
+        renderQuestion()
+        restartTimer()
+        incorrectAnswers++
+
+    }
+}
+
+function restartTimer() {
+    number = 3
+    run()
+
+}
+
+function stop() {
+    clearInterval(startGame);
+} 
+
+
+$("#answers").on("click", ".answer-button", function(){
+    var userClicked = $(this).data("name")
+        if(userClicked === question[questionIndex].c){
+            console.log("correct!")
+        var correctImage = $("<img>")
+        console.log(question[questionIndex].c)
+        correctImage.attr("src", question[questionIndex].image)
+        $(".jumbotron").append(correctImage)
+        correctAnswers++;
+        questionIndex++;
         renderQuestion();
-        startGame = setInterval(decrement, 1000);
+       
+       
     }
-
-
-    function decrement (){
-        number--;
-        $("#showNumber").html(number);
-        console.log(number);
-        if (number === 0){
-            stop();
-            alert("Time Up!");
-            questionIndex++
-            renderQuestion()
-            restartTimer()
-            incorrectAnswers ++
-            console.log(incorrectAnswers);
-        }
-    }
-
-    function restartTimer(){
-        number=1
-        run()
-
-    }
-
-    function stop (){
-        clearInterval(startGame);
-    } 
+})
